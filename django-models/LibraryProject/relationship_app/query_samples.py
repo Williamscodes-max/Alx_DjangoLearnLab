7069546f2fd4fee3b0b41dcf68978b2579a4d0d2
williams alayode <walayode@gmail.com>
2026-01-15 23:34:01 +0100

    Add required ORM queries for model relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,14 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "John Doe"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print(books_by_author)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print(books_in_library)
 
 # Retrieve the librarian for a library
-librarian = library.librarian
-print(librarian)
+librarian = Librarian.objects.get(library=library)
