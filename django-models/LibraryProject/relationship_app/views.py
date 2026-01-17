3254ec13ff457667febefa8298908a39ead4a0be
williams alayode <walayode@gmail.com>
2026-01-17 13:37:33 +0100

    Implement member role-based access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.decorators import user_passes_test
 from .models import UserProfile
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return (
+        user.is_authenticated and
+        hasattr(user, 'userprofile') and
+        user.userprofile.role == 'Member'
+    )
 
 @user_passes_test(is_member)
 def member_view(request):
