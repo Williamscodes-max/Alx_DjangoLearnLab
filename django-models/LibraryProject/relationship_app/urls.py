d8ba7f0dc2bf1e5dc7d40184510e347a2caea3a5
williams alayode <walayode@gmail.com>
2026-01-17 18:34:42 +0100

    Implement custom permissions for Book model

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -36,9 +36,18 @@
 #     path('member/', member_view, name='member_view'),
 # ]
 
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('member/', views.member_view, name='member'),
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('member/', views.member_view, name='member'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from .models import UserProfile
+from django.contrib.auth.decorators import permission_required
 
 # ===== Role check helpers =====
 
@@ -38,3 +39,19 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    return render(request, 'relationship_app/edit_book.html')
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    return render(request, 'relationship_app/delete_book.html')
