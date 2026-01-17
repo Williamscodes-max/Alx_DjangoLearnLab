91b3310a00bd9e3f3a36e2f22365c8d20fb9779d
williams alayode <walayode@gmail.com>
2026-01-17 13:10:27 +0100

    Implement member role-based access control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,15 +24,21 @@
 #     ),
 # ]
 
-from django.urls import path
+# from django.urls import path
 
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
+# from .admin_view import admin_view
+# from .librarian_view import librarian_view
+# from .member_view import member_view
+
+# urlpatterns = [
+#     path('admin/', admin_view, name='admin_view'),
+#     path('librarian/', librarian_view, name='librarian_view'),
+#     path('member/', member_view, name='member_view'),
+# ]
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('member/', views.member_view, name='member'),
 ]
-
