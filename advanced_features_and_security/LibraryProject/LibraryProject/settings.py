f0e23eca38d95efab932276633e5a6888a7524d5
Williamscode-max <walayode@gmail.com>
2026-01-22 08:33:09 -0800

    updated the files

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 
 
@@ -7,3 +9,17 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author")
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['user', 'email', 'date_of_birth', 'is_staff', 'is_active']
+    fieldsets = UserAdmin + fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields' ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.regiester(CustomUser, CustomUserAdmin)
