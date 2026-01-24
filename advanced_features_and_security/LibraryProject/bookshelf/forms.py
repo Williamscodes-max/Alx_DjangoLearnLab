1facb89e28c33580a88150ac807dcf98557e0769
williams alayode <walayode@gmail.com>
2026-01-24 13:29:01 +0100

    Add ExampleForm and update templates/views for ALX

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,7 +2,13 @@ from django import forms
 from .models import Book
 
 # Form to create/edit a Book
-class BookForm(forms.ModelForm):
+# class BookForm(forms.ModelForm):
+#     class Meta:
+#         model = Book
+#         fields = ['title', 'author', 'publication_year']
+
+
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
